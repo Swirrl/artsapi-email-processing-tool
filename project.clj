b56de37b3d502ba1dec853e93ae62439e6d368ff
Alex Lynham <acelynham@hotmail.com>
2014-11-25 16:00:37 +0000

    First commit.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject artsapi-graft "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [grafter "0.2.0-SNAPSHOT"]
+                 [clojure-mail "0.1.6"]
+                 [org.mnode.mstor/mstor "0.9.13"]])
---
+++ test/artsapi_graft/core_test.clj
@@ -0,0 +1,7 @@
+(ns artsapi-graft.core-test
+  (:require [clojure.test :refer :all]
+            [artsapi-graft.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
