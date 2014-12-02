2b9a4be05c90821a01e1abf9334fb63451032b40
Alex Lynham <acelynham@hotmail.com>
2014-12-02 17:24:19 +0000

    Work in progress on resource ids from updated ontology.

--- src/artsapi_graft/email.clj
+++ src/artsapi_graft/email.clj
@@ -2,10 +2,6 @@
   (:require [artsapi-graft.ontologies :refer :all]
             [artsapi-graft.prefixers :refer :all]))
 
-(defn generate-email-uri
-  "Pass this an email map and have a unique graph uri returned for the email in question."
-  [email])
-
 (defn generate-mailto-uri
   "Pass this a string containing an email address and have a mailto string returned."
   [address]
--- src/artsapi_graft/prefixers.clj
+++ src/artsapi_graft/prefixers.clj
@@ -4,14 +4,31 @@
 
 (def base-graph (prefixer (arts "graph/")))
 
-(def organisation-graph-uri (prefixer (base-graph "organisation/")))
+(def organisation-graph-uri (prefixer (base-graph "organisations")))
 
-(def person-graph-uri (prefixer (base-graph "person/")))
+(def project-graph-uri (prefixer (base-graph "projects")))
 
-(def email-graph-uri (prefixer (base-graph "email/")))
+(def domain-graph-uri (prefixer (base-graph "domains")))
 
-(def organisation-uri)
+(def person-graph-uri (prefixer (base-graph "people")))
 
-(def person-uri)
+(def email-graph-uri (prefixer (base-graph "emails")))
 
-(def email-uri)
+(defn ->slug
+  [id-string])
+
+(defn resource-uri
+  "Generate a resource uri. For example, these outputs should be expected:
+   ['people' 'Jeff Lebowski] ;=>
+   ['email' 'jeff@example.com'] ;=>
+   ['domain' 'swirrl.com'] ;=> "
+  [graph id-string]
+  (str (prefixer (base-graph "graph/id/"))
+       (->slug id-string)))
+
+(defn user-account
+  "Generate a uri for a user's account. These are nested under the resource uri for the person
+   that owns the account instance.
+   Example: ['Jeff Lebowski' :email] ;=> domain/graph/people/jeff-lebowski/accounts/email"
+  [user-slug account])
+
