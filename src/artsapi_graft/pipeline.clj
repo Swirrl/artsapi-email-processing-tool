c0a6bd6e33a3ef292f6e404d045c8d32f1cb3695
Alex Lynham <acelynham@hotmail.com>
2014-12-03 14:44:43 +0000

    Fixed syntax error.

--- src/artsapi_graft/pipeline.clj
+++ src/artsapi_graft/pipeline.clj
@@ -1,9 +1,17 @@
 (ns artsapi-graft.pipeline
   (:require [grafter.tabular :refer :all]
-            [artsapi-graft.store :refer :all]))
+            [artsapi-graft.store :refer :all]
+            [artsapi-graft.ontologies :refer :all]
+            [artsapi-graft.prefixers :refer :all]))
 
 (defn make-email-ds
-  )
+  "Get all messages from the supplied store. Tries the default folder, inbox and sent folders.
+   The store should correspond to a single instance of an mbox, created using artsapi-graft.store/store"
+  [store]
+  (-> (->> [:default "inbox" "sent"] (get-all-messages store)
+           (make-dataset [:from :from-personal :from-domain
+                          (:to :personal) (:to :email) (:to :domain)
+                          :subject (:cc :personal) (:cc :email) (:cc :domain)]))))
 
 (defn email-pipeline
   [dataset]
