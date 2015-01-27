aa532b220e09c423d642ac44223eaf3e833966dc
Alex Lynham <acelynham@hotmail.com>
2015-01-27 16:04:57 +0000

    Bumped grafter version.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [grafter "0.3.0-SNAPSHOT"]
+                 [grafter "0.3.0"]
                  [org.mnode.mstor/mstor "0.9.13"]
                  [digest "1.4.4"]
                  [cheshire "5.4.0"]]
